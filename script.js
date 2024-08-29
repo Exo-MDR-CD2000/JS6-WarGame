4659f665b1bf9124a49efb9c4f40f282cab8c2e7
JAG24_Exo <joseguillen587@yahoo.com>
2024-08-29 13:36:11 -0700

    template html & js files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Consider classes such as: Card, Deck, Player, as well as what properties and methods they may include.
+
+// Four suits to represent the appearance (user interface - ui) for your cards
+let cardSuits = ["Spades 🗡️", "Hearts ❤️", "Diamonds 💎", "Clubs 🍀"];
+console.log("Card Suits Example:", cardSuits); 
